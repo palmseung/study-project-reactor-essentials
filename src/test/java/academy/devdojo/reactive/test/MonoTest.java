b90b83c78bb74f98b89f4ebc3a4dfd7e45329c45
palmseung <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>
2022-06-18 02:06:58 +0900

    [02] Quick recap about Reactive Streams

--- src/test/java/academy/devdojo/reactive/test/MonoTest.java
+++ src/test/java/academy/devdojo/reactive/test/MonoTest.java
@@ -4,6 +4,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
+/*
+ * Reactive Streams
+ * 1. Asynchronous
+ * 2. Non-blocking
+ * 3. Backpressure
+ * Publisher <- (subscribe) Subscriber
+ * Subscription is created
+ * Publisher (onSubscribe with the subscription) -> Subscriber
+ * Subscription <- (request N) Subscriber
+ * Publisher <- (onNext) Subscriber
+ * Until,
+ * 1. Publisher send all the objects requested.
+ * 2. Publisher send all the objects it has. (onComplete) subscriber and subscription will be canceled.
+ * 3. There is an error. (onError) subscriber and subscription will be canceled.
+ *
+ */
 public class MonoTest {
 
     @Test
