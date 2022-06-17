69d5876d8bebe9a19a87397f51412b16021e073a
palmseung <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>
2022-06-18 01:28:41 +0900

    [01] Project Setup

---
+++ src/test/java/academy/devdojo/reactive/test/MonoTest.java
@@ -0,0 +1,14 @@
+package academy.devdojo.reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class MonoTest {
+
+    @Test
+    void test() {
+        log.info("Everything working as intended...!!!");
+    }
+
+}
